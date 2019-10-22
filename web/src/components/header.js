51042c66022b7ff1f9aed606981052bb0d455326
Jarett Wright <jwdevguy@gmail.com>
2019-10-21 21:50:30 -0500

    added logo

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -9,7 +9,10 @@ const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
   <div className={styles.root}>
     <div className={styles.wrapper}>
       <div className={styles.branding}>
-        <Link to='/'>{siteTitle}</Link>
+        
+        <Link to='/'>
+        <img src="https://res.cloudinary.com/jwkctech/image/upload/v1571712317/method-draw-image.svg" width="200px" />
+        </Link>
       </div>
 
       <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
